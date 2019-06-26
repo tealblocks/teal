2c0eddf4225ebfb8b22d54bd7fb703aaa30c8eff
tensoras <ab@tensoras.com>
2019-06-26 13:27:25 +0000

    config changes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -95,7 +95,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "a.seed.tealblocks.com:8080",
-  "b.seed.tealblocks.com:8081",
+  "b.seed.tealblocks.com:8080",
 };
 
 struct CheckpointData {
